e7ef99b6a771f487532e9d05feff05cce3fc8ea6
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-27 20:46:50 +0530

    Update fsub_sql.py

--- hellbot/sql/fsub_sql.py
+++ hellbot/sql/fsub_sql.py
@@ -1,15 +1,22 @@
-from . import SESSION, BASE
-from sqlalchemy import Column, String
-
+import threading
+from sqlalchemy import Column, String, UnicodeText, distinct, func
+from . import BASE, SESSION
 
 class Fsub(BASE):
     __tablename__ = "fsub"
     chat_id = Column(String(14), primary_key=True)
-    usrname = Column(String(14), primary_key=True)
+    hunter = Column(String(14), primary_key=True, nullable=False)
 
-    def __init__(self, chat_id, usrname):
+    def __init__(self, chat_id, hunter):
         self.chat_id = str(chat_id)
-        self.usrname = str(usrname)
+        self.hunter = str(hunter)
+  
+    def __eq__(self, other):
+        return bool(
+            isinstance(other, Fsub)
+            and self.chat_id == other.chat_id
+            and self.hunter == other.hunter
+        )
 
 
 Fsub.__table__.create(checkfirst=True)
@@ -24,8 +31,8 @@ def is_fsub(chat_id):
         SESSION.close()
 
 
-def add_fsub(chat_id, usrname):
-    adder = Fsub(str(chat_id), str(usrname))
+def add_fsub(chat_id, hunter):
+    adder = Fsub(str(chat_id), str(hunter))
     SESSION.add(adder)
     SESSION.commit()
 
