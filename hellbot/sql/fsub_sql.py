2195ddc69d050f13299b7d656886862440d9e334
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-06-30 12:58:04 +0530

    Update fsub_sql.py

--- hellbot/sql/fsub_sql.py
+++ hellbot/sql/fsub_sql.py
@@ -1,50 +1,42 @@
-import threading
-from sqlalchemy import Column, String, UnicodeText, distinct, func
-from . import BASE, SESSION
+from sqlalchemy import Column, String, Numeric, Boolean
+from . import SESSION, BASE
 
-class Fsub(BASE):
-    __tablename__ = "fsub"
-    chat_id = Column(String(14), primary_key=True)
-    hunter = Column(String(14), primary_key=True, nullable=False)
+class forceSubscribe(BASE):
+    __tablename__ = "forceSubscribe"
+    chat_id = Column(Numeric, primary_key=True)
+    channel = Column(String)
 
-    def __init__(self, chat_id, hunter):
-        self.chat_id = str(chat_id)
-        self.hunter = str(hunter)
-  
-    def __eq__(self, other):
-        return bool(
-            isinstance(other, Fsub)
-            and self.chat_id == other.chat_id
-            and self.hunter == other.hunter
-        )
+    def __init__(self, chat_id, channel):
+        self.chat_id = chat_id
+        self.channel = channel
 
 
-Fsub.__table__.create(checkfirst=True)
+forceSubscribe.__table__.create(checkfirst=True)
 
 
 def is_fsub(chat_id):
     try:
-        return SESSION.query(Fsub).filter(Fsub.chat_id == str(chat_id)).one()
-    except BaseException:
+        return SESSION.query(forceSubscribe).filter(forceSubscribe.chat_id == chat_id).one()
+    except:
         return None
     finally:
         SESSION.close()
 
 
-def add_fsub(chat_id, hunter):
-    adder = Fsub(str(chat_id), str(hunter))
+def add_fsub(chat_id, channel):
+    adder = SESSION.query(forceSubscribe).get(chat_id)
+    if adder:
+        adder.channel = channel
+    else:
+        adder = forceSubscribe(
+            chat_id,
+            channel
+        )
     SESSION.add(adder)
     SESSION.commit()
 
-
-def rem_fsub(chat_id):
-    rem = SESSION.query(Fsub).get(str(chat_id))
+def rm_fsub(chat_id):
+    rem = SESSION.query(forceSubscribe).get(chat_id)
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
-
-
-def all_fsub():
-    rem = SESSION.query(Fsub).all()
-    SESSION.close()
-    return rem
