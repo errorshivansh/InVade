886d30760df51482ee296291e09d02328481f84c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-07-03 14:27:01 +0530

    Update pmpermit.py

--- hellbot/plugins/pmpermit.py
+++ hellbot/plugins/pmpermit.py
@@ -268,7 +268,7 @@ if PM_ON_OFF != "DISABLE":
                 pass
         
         botusername = Config.BOT_USERNAME
-        tap = await bot.inline_query(botusername, "pm_warn")
+        tap = await event.client.inline_query(botusername, "pm_warn")
         hel_ = await tap[0].click(event.chat_id)
         PM_WARNS[chat_ids] += 1
         if chat_ids in PREV_REPLY_MESSAGE:
